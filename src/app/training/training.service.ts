a9f6da73e91175540f7b0592e02d5b077876fbd8
Nikos <tsounis@sabern.com>
2018-02-09 00:23:24 +0100

    -Material design data table

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -37,7 +37,7 @@ export class TrainingService {
         this.exercises.push({
             ...this.runningExercise,
             duration: this.runningExercise.duration * (progress / 100),
-            calories: this.runningExercise.duration * (progress / 100),
+            calories: this.runningExercise.calories * (progress / 100),
             date: new Date(),
             state: 'cancelled'
         });
@@ -47,5 +47,9 @@ export class TrainingService {
 
     getRunnicngExercise() {
         return { ...this.runningExercise };
+    }
+
+    getCompletedOrCancelledExercises() {
+        return this.exercises.slice();
     }
 }
