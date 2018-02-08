519db7327d8d8fc20b20f208bbd0234ae1d86a96
Nikos <tsounis@sabern.com>
2018-02-09 00:02:07 +0100

    complete and cancel buttons

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthGuard {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (this.authService.isAuth()) {
+            return true;
+        } else {
+            this.router.navigate(['/login']);
+        }
+    }
+}
